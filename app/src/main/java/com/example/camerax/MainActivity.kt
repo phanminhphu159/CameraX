f31388b5307d08a0056e818316325da5379bda69
manle1003 <lehongman0909@gmail.com>
2023-04-05 15:47:46 +0700

    hi abc

--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -25,7 +25,7 @@ import java.util.concurrent.Executors
 typealias LumaListener = (luma: Double) -> Unit
 // hi
 // hi man, 123
-// hi phu
+// hi phu, 123
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
 
