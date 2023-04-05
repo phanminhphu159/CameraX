4083a9a5c33f7ef68eca94f4c44aaa26873a0428
manle1003 <lehongman0909@gmail.com>
2023-04-05 15:59:57 +0700

    Add new feature
    Signed-off-by: phanminhphu159 <phanminhphu159@gmail.com>

--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -25,7 +25,7 @@ import java.util.concurrent.Executors
 typealias LumaListener = (luma: Double) -> Unit
 // hi
 // hi man, 123
-// hi phu, 123, 123445
+// hi phu, 123, 1234457
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
 
